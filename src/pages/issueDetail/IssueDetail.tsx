8a509aaa78e390c6d76e1b114b0c02e06d5a2047
prefer_choi <prefer_choi@naver.com>
2023-09-01 11:54:18 +0900

    fix:main과 병합해서 분되는 부분 처리

--- src/pages/issueDetail/IssueDetail.tsx
+++ src/pages/issueDetail/IssueDetail.tsx
@@ -1,11 +1,26 @@
+import React, { useEffect } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
 import { issueDetailResponse } from '../../services/getIssueDataByOctokit';
+import './IssueDetail.css';
+import Prism from 'prismjs';
 
 const IssueDetail = () => {
   const response = useLoaderData() as issueDetailResponse;
   const issueDetail = response.data;
 
-  return <div>IssueDetail</div>;
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [issueDetail]);
+
+  return (
+    <div className="IssueDetail" style={{ border: '4px solid black', padding: '5%' }}>
+      {/* 여기 헤더 투입 */}
+      <img src={issueDetail?.user?.avatar_url || ''} alt="user avatar" />
+      {/* list cell 형태 삽입 */}
+      <ReactMarkdown>{issueDetail?.body || ''}</ReactMarkdown>
+    </div>
+  );
 };
 
 export default IssueDetail;
