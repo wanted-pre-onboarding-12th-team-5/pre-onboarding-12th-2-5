abbffe6000152bc4f75e97d897b6f658625f1432
chichoon <chichoon.choi@gmail.com>
2023-09-01 12:50:53 +0900

    feat: :sparkles: 무한스크롤 부착

--- src/pages/issueList/IssueList.tsx
+++ src/pages/issueList/IssueList.tsx
@@ -5,11 +5,13 @@ import { getIssuesList, issueListResponse } from 'services/getIssueDataByOctokit
 import { IssueType } from 'types/issue';
 import AdvertiseElement from './AdvertiseElement';
 import IssueElement from './IssueElement';
+import { useIntersect } from 'hooks/useIntersect';
 
 const IssueList = () => {
   const response = useLoaderData() as issueListResponse;
   const [issueList, setIssueList] = useState<IssueType[]>([...response.data]);
   const [page, setPage] = useState(1);
+  const intersectionObserverTarget = useIntersect(handleGetMoreIssues);
 
   async function handleGetMoreIssues() {
     await getIssuesList(page + 1).then(response => {
@@ -28,9 +30,11 @@ const IssueList = () => {
               <IssueElement issue={issue} />
             </Fragment>
           ) : (
-            <IssueElement issue={issue} />
+            <IssueElement key={issue.number} issue={issue} />
           ),
         )}
+        {issueList.length > 0 && <div style={{ height: 100 }} ref={intersectionObserverTarget} />}
+        {/* TODO: 스타일 적용 시에 div style: height 100px 적용 */}
       </ul>
     </React.Suspense>
   );
