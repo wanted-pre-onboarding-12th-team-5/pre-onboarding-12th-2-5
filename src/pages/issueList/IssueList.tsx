62fb6b2583ca93593629b634e754b3e31488f2ff
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-09-01 12:03:10 +0900

    feat:issueElement,advertiseElement 컴포넌트 생성 및 Suspense 작업중

--- src/pages/issueList/IssueList.tsx
+++ src/pages/issueList/IssueList.tsx
@@ -1,12 +1,27 @@
+import React from 'react';
 import { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { issueListResponse } from '../../services/getIssueDataByOctokit';
+import AdvertiseElement from './AdvertiseElement';
+import IssueElement from './IssueElement';
 
 const IssueList = () => {
   const response = useLoaderData() as issueListResponse;
   const [issueList, setIssueList] = useState([...response.data]);
 
-  return <div>IssueList</div>;
+  return (
+    <React.Suspense fallback={<p>Loading Test</p>}>
+      <ul>
+        {issueList.map((issue, index) => (
+          //FIXME
+          <>
+            <IssueElement />
+            <AdvertiseElement />
+          </>
+        ))}
+      </ul>
+    </React.Suspense>
+  );
 };
 
 export default IssueList;
